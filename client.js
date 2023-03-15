db7d92b0c58ae4715e699f9e8d8607d0cd8e7588
Kade Kobussen <kadekobussen50@gmail.com>
2023-03-15 12:47:20 -0700

    changed imports and exports to es6 shorthand and made client.js

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require('net');
+
+function connect() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // define event listener for incoming data
+  conn.on('data', (data) => {
+    console.log('data:', data);
+  });
+  conn.on('connect', () => {
+    console.log('snake initiated');
+  });
+  conn.on('connect_timeout', () => {
+  });
+
+  return conn;
+}
+
+
+
+
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,26 +1,6 @@
 const net = require("net");
+const {connect} = require("./client.js")
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // define event listener for incoming data
-  conn.on('data', (data) => {
-    console.log('data:', data);
-  });
-  conn.on('connect', () => {
-    console.log('snake initiated')
-  });
-  conn.on('connect_timeout', () => {
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
